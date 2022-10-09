b54fe0ff8520c2df23bb16fc631d6ec538c0c7d8
CaptainHarlockSSX <thedarklink12mc@gmail.com>
2022-10-09 23:08:38 +0200

    Working face detection machine learning algorithm
    - Add a dedicated file to train a model (modelTraining.py)
    - Predict people names from a test set (faceDetectionML.py)

--- src/faceDetectionML.py
+++ src/faceDetectionML.py
@@ -1,42 +1,88 @@
 import sys
 import numpy as np
+import re
 import glob
 import cv2
 import tensorflow as tf
 
+# Table associating a number to a name
+labelID = []
+
+# Regex to get the name of the person on the image, based on the filename
+def getLabel(fileName):
+    pattern = re.compile('^(.*\/)*(?P<name>.+)(_[0-9]*\.jpeg){1}$')
+    found = pattern.match(fileName)
+    if found:
+        return found.groupdict()['name']
+    else:
+        return ""
+
+
 # Load all images from a database - Return a training set and a testing set
 def loadImageDatabase():
     # Initialize two array
-    trainSet = []
-    testSet = []
+    testSet, testLabels = [], []
 
     # Load arrays with images if the size is 2000x2000 px
-    for img in glob.glob('../../Test_Set/*R.jpeg'):
+    for img in glob.glob('../../Test_Set/*.jpeg'):
         image = cv2.imread(img, 0)
         if image.shape == (2000,2000):
             testSet.append(image)
+            name = getLabel(img) # Extract name of the person from the filename
 
-    for img in glob.glob('../../Train_Set/*R.jpeg'):
-        image = cv2.imread(img, 0)
-        if image.shape == (2000,2000):
-            trainSet.append(image)
+            if name not in labelID:
+                labelID.append(name)
+
+            testLabels.append(labelID.index(name))
 
     # Convert arrays to numpy arrays
-    trainSet = np.array(trainSet, dtype = int)
-    testSet = np.array(testSet, dtype = int)
+    testSet = np.array(testSet, dtype = np.uint8)
+    testLabels = np.asarray(testLabels)
 
     # Scale colors from 0 - 255 to 0 - 1
-    trainSet = trainSet / 255.0
     testSet = testSet / 255.0
 
-    return (trainSet, testSet)
+    return (testSet, testLabels)
+
+##################### main #####################
 
 # Load data sets
-(trainSet, testSet) = loadImageDatabase()
-print(trainSet.shape)
-print(testSet.shape)
+(testSet, testLabels) = loadImageDatabase()
+
+# # print(trainSet.shape)
+# # print(testSet.shape)
+#
+# # cv2.imshow("Tronche", trainSet[5])
+# # print(trainLabels[5])
+# #
+# while(True):
+#     if cv2.waitKey(10) == 27:
+#         break
+
+# Get saved ML model
+model = tf.keras.models.load_model('../../SavedModels/faceRecognitionModel')
+
+# Build the probability model to process predictions on chosen images
+probabilityModel = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+
+# Gather the predictions values
+predictions = probabilityModel.predict(testSet)
+
+for i in range(len(testSet)):
+    bestPrediction = np.argmax(predictions[i])
+    guess = labelID[bestPrediction]
+    img = testSet[i]
+    cv2.putText(img=img, text='I think you are '+ guess , org=(400, 1600), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=3, color=(255, 0, 255),thickness=3)
+    cv2.putText(img=img, text='Press space to continue...', org=(800, 1800), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1, color=(255, 0, 255),thickness=3)
+    cv2.imshow("Heads", img)
+
+    # Press spacebar to continue
+    while(True):
+        if cv2.waitKey(0) == 32:
+            break;
 
 print("OK")
+
 # define a video capture object
 # device = cv2.VideoCapture(0)
 
@@ -48,7 +94,6 @@ print("OK")
 
     # if cv2.waitKey(10) == 27:
     #     break
-#
-# device.release()
-# sys.exit() # to exit from all the processes
-# cv2.destroyAllWindows() # destroy all windows
+
+sys.exit() # Exit from all the processes
+cv2.destroyAllWindows() # Destroy all windows
