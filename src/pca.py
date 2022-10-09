366749557dd020f1586faa70fcefdbe35a6d92c1
Thomas Bédrine <thomas.bedrine@obspm.fr>
2022-10-09 18:27:12 +0200

    Calculate eigen vectors for the PCA

--- src/pca.py
+++ src/pca.py
@@ -3,7 +3,7 @@ import numpy as np
 import glob
 import cv2
 
-#  read and image
+# Pick images from dataset and load them
 
 
 def loadImageDatabase():
@@ -15,27 +15,50 @@ def loadImageDatabase():
     return faces
 
 
-def matrixToLine(faces):
-    matrix = []
+def picturesToLines(faces):
+    # M pixels (width*height = M) times N pics
+    pics = []
     for face in faces:
-        matrix.append(face.flatten())
-    return matrix
+        # Each pic become one line of the MxN matrix
+        pics.append(face.flatten())
+    pics = np.array(pics).transpose()
+    return pics
+
+
+def findEigenvectors(mat):
+    # Substracting the mean vector to all vectors
+    subMatrix = mat - mat.mean(axis=0, keepdims=True)
+
+    # Pseudo-covariance matrix : transposed C dot C (CtC)
+    covarMatrix = subMatrix.transpose()@subMatrix
+    # w : eigenvalues of CtC, v : eigenvectors of CtC
+    w, v = np.linalg.eig(covarMatrix)
+    # Eigenvectors of CCt : C dot v
+    eigenV = subMatrix@v
+    normEigenV = eigenV/np.linalg.norm(eigenV)
+
+    return normEigenV
 
 
 faces = loadImageDatabase()
-matrix = matrixToLine(faces)
+pics = picturesToLines(faces)
+test = np.array([[1, 2, 3, 4, 5, 6, 7, 8, 9], [4, 5, 6, 7, 8,
+                9, 10, 11, 12], [7, 8, 9, 10, 11, 12, 13, 14, 15]]).transpose()
+print(test)
+test2 = findEigenvectors(test)
+print(test2)
 
 # define a video capture object
 # device = cv2.VideoCapture(0)
 
-while True:
-    # ret, currentFrame = device.read()
-    # currentFrame = cv2.flip(currentFrame, 1)
-    #
-    # cv2.imshow("Camera Stream", currentFrame)
+# while True:
+#     # ret, currentFrame = device.read()
+#     # currentFrame = cv2.flip(currentFrame, 1)
+#     #
+#     # cv2.imshow("Camera Stream", currentFrame)
 
-    if cv2.waitKey(10) == 27:
-        break
+#     if cv2.waitKey(10) == 27:
+#         break
 #
 # device.release()
 # sys.exit() # to exit from all the processes
